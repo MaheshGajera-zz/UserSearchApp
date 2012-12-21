b71ee9abf49edebe235ab9a043efb0ee5d5dcf91
Mahesh Gajera <mahesh.gajera2012@gmail.com>
2012-12-21 14:13:48 -0500

    addining intial version of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserSearch::Application.config.secret_token = 'f4c734a2161e5d456f8659d378a78071e584c67295e53930327b118c9046a69b7a10633a73274ab27fc2ae40a8869f4b13bba7cdf0fe38d1724576c20a543dcd'
