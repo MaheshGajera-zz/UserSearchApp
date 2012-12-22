d5836beae83cbb99dd6109c8065b49fc24b72713
Mahesh Gajera <mahesh.gajera2012@gmail.com>
2012-12-22 10:14:48 -0500

    Adding navigation stuffs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,15 @@ class TasksController < ApplicationController
   	def index
   		@tasks = Task.all
   	end
+
   	def new
   		@task = Task.new
   	end
+
   	def show
     	@task = Task.find(params[:id])
   	end
+    
   	def create
 	    @task = Task.new(params[:task])
 	    if @task.save
