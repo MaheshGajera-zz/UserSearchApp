807b21ea7785aa01ee9bfe8a433bc34a7767ceb9
Mahesh <mahesh@farematrix.com>
2012-12-27 20:17:11 +0530

    adding filters and sorting

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,18 @@
 class TasksController < ApplicationController
   	def index
-        unless params[:title].nil? || params[:title].blank?
+        #-- Load As per Filters
+        if ! params[:title].nil?
           @tasks = Task.find(:all, :conditions => ['title LIKE ?', "%#{params[:title]}%"])
+        elsif ! params[:overdue].nil?
+          @tasks = Task.find(:all, :conditions => ['timing  > ?', DateTime.now ] )
         else
           @tasks = Task.all
-  		end
+        end
+        
+        #-- Apply Sorting If requested
+        if has_sorting?
+          @tasks = sort_results( @tasks )
+        end
   	end
 
   	def new
@@ -47,5 +55,34 @@ class TasksController < ApplicationController
       
       flash[:success] = "Task deleted."
       redirect_to action: "index"
-  	end
+    end
+    
+    private
+    
+    def has_sorting?
+      unless params[:priority].nil?
+        return true
+      end
+      unless params[:timing].nil?
+        return true
+      end
+      return false
+    end
+    
+    def sort_results(tasks)
+      unless params[:priority].nil?
+          priority_map = {
+          'High'   => 1,
+          'Medium' => 2,
+          'Low'    => 3,
+        }
+        tasks = tasks.sort! { |a, b|  priority_map[a.priority] <=> priority_map[b.priority] }
+      end
+      
+      unless params[:timing].nil?
+        tasks = tasks.sort! { |a, b|  a.timing <=> b.timing }
+      end
+      
+      return tasks
+    end
 end
