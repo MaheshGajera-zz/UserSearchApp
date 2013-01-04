0068c1e91f46c6523aaccfc0232725ea8e7b424f
Mahesh <mahesh@farematrix.com>
2013-01-04 16:39:07 +0530

    adding task integration to user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,14 @@
 class TasksController < ApplicationController
     before_filter :signed_in_user
-    before_filter :correct_user
     
   	def index
         #-- Load As per Filters
         if ! params[:title].nil?
-          @tasks = Task.matching_title(params[:title])
+          @tasks = current_user.tasks.matching_title(params[:title])
         elsif ! params[:overdue].nil?
-          @tasks = Task.non_over_due
+          @tasks = current_user.tasks.non_over_due
         else
-          @tasks = Task.all
+          @tasks = current_user.tasks.all
         end
         
         #-- Apply Sorting If requested
@@ -17,16 +16,16 @@ class TasksController < ApplicationController
   	end
 
   	def new
-  		@task = Task.new
+  		@task = current_user.tasks.new
   	end
     
     def edit
-    	@task = Task.find(params[:id])
+    	@task = current_user.tasks.find(params[:id])
   	end
     
     def update
-      @task = Task.find(params[:id])
-      
+      @task = current_user.tasks.find(params[:id])
+
       if @task.update_attributes( params[:task] )
         flash[:success] = "Task Updated."
         redirect_to action: 'index'
@@ -37,8 +36,7 @@ class TasksController < ApplicationController
   	end
     
   	def create
-      @task = Task.new(params[:task])
-	    
+      @task = current_user.tasks.new(params[:task])
       if @task.save
 	    flash[:success] = "Task created successfully."
 	    redirect_to action: "index"
@@ -48,11 +46,13 @@ class TasksController < ApplicationController
   	end
   	
     def destroy
-      Task.find(params[:id]).destroy
+      current_user.tasks.find(params[:id]).destroy
       
       flash[:success] = "Task deleted."
       redirect_to action: "index"
     end
+    
+    
     
     private
     
@@ -80,8 +80,4 @@ class TasksController < ApplicationController
       end
     end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(tasks_path) unless current_user?(@user)
-    end
 end
